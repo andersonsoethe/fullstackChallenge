136629c600108dc18534c5ef57ff7979d7e2eacd
gutosoethe <gutosoethe@gmail.com>
2025-01-04 22:29:41 -0300

    spring initialzr

---
+++ src/test/java/com/gutosoethe/fullstackChallenge/FullstackChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gutosoethe.fullstackChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
